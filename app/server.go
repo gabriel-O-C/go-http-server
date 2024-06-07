6f5412c0eda0fe5602a9fc9cf4bf3a333ddf39f5
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2024-06-07 11:25:35 -0300

    pass 1st stage

--- app/server.go
+++ app/server.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"net/http"
 	"os"
 )
 
+func Handler(conn net.Conn) {
+	defer conn.Close()
+
+	request, err := http.ReadRequest(bufio.NewReader(conn))
+	if err != nil {
+		fmt.Println("Error reading request", err.Error())
+		return
+	}
+
+	if request.URL.Path == "/" {
+
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		return
+	}
+
+	conn.Write([]byte("HTTP/1.1 404 NOT FOUND\r\n\r\n"))
+}
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
@@ -21,5 +40,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	c.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	Handler(c)
+
 }
